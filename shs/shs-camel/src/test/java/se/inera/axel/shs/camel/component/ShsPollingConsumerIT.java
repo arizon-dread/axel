a0eb94443df930860a794be529acc8835180a024
Bjorn Bength <bjorn@bength.se>
2015-11-09 15:05:04 +0100

    More tests of camel polling consumer endpoint.

--- shs/shs-camel/src/test/java/se/inera/axel/shs/camel/component/ShsPollingConsumerIT.java
+++ shs/shs-camel/src/test/java/se/inera/axel/shs/camel/component/ShsPollingConsumerIT.java
@@ -21,6 +21,7 @@ package se.inera.axel.shs.camel.component;
 import org.apache.camel.EndpointInject;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.apache.camel.builder.PredicateBuilder;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.impl.JndiRegistry;
@@ -30,18 +31,25 @@ import org.springframework.test.context.ContextConfiguration;
 import org.testng.annotations.Test;
 import se.inera.axel.shs.client.ShsClient;
 import se.inera.axel.shs.processor.ShsHeaders;
+import se.inera.axel.shs.processor.ShsMessageMarshaller;
+import se.inera.axel.shs.xml.label.SequenceType;
+import se.inera.axel.shs.xml.label.Status;
+import se.inera.axel.shs.xml.label.TransferType;
 import se.inera.axel.shs.xml.message.Message;
 import se.inera.axel.shs.xml.message.ShsMessageList;
 
+import java.util.HashMap;
+
+import static com.natpryce.makeiteasy.MakeItEasy.*;
+import static se.inera.axel.shs.mime.ShsMessageMaker.ShsMessage;
+import static se.inera.axel.shs.xml.label.ShsLabelMaker.*;
+import static se.inera.axel.shs.xml.label.ShsLabelMaker.ShsLabelInstantiator.*;
+
 @ContextConfiguration
 public class ShsPollingConsumerIT extends CamelTestSupport {
-//	@Produce(uri = "direct:start")
-//	ProducerTemplate producer;
-//
+
 	@EndpointInject(uri = "mock:result")
 	MockEndpoint resultEndpoint;
-
-    //ShsClient client = new ShsClient();
 
     @Override
     protected JndiRegistry createRegistry() throws Exception {
@@ -64,44 +72,90 @@ public class ShsPollingConsumerIT extends CamelTestSupport {
 			@Override
 			public void configure() throws Exception {
 
-				from("shs:client?to=0000000000.jmeter&status=test&producttype=00000000-0000-0000-0000-000000000000")
-                .to("log:se.inera.axel.ShsPollingConsumerIT?showAll=true")
-                .process(new Processor() {
-                    @Override
-                    public void process(Exchange exchange) throws Exception {
-                     //   throw new RuntimeException("nu vart det error in batch index " + exchange.getProperty(Exchange.BATCH_INDEX));
-                    }
-                })
+				from("shs:client?to=0000000000.junit&producttype=00000000-0000-0000-0000-000000000000")
                 .setHeader(Exchange.FILE_NAME, header(ShsHeaders.TXID))
-                .to("file:/tmp/out")
                 .to("mock:result");
 
 
-                ShsMessageList list = new ShsMessageList();
-                Message m = new Message();
-                m.setTo("0000000000.jmeter");
-                m.setProduct("error");
-                m.setTxId("88B06FFC-860D-430F-B812-A0DA2EB40E9F");
+                final HashMap<String, se.inera.axel.shs.mime.ShsMessage> messages = new HashMap();
 
-                list.getMessage().add(m);
+                se.inera.axel.shs.mime.ShsMessage shs1 = make(an(ShsMessage,
+                    with(ShsMessage.label, an(ShsLabel,
+                        with(txId, "88B06FFC-860D-430F-B812-A0DA2EB40E9F"),
+                        with(status, Status.TEST),
+                        with(to, a(To, with(To.value, "0000000000.jmeter"))),
+                        with(product, a(Product, with(Product.value, "00000000-0000-0000-0000-000000000000"))),
+                        with(sequenceType, SequenceType.EVENT),
+                        with(transferType, TransferType.ASYNCH)))));
+
+
+                messages.put(shs1.getLabel().getTxId(), shs1);
+
                 /* mocking shs server */
-                from("jetty:http://localhost:8585/shs/ds?matchOnUriPrefix=true" )
-                        .setBody(constant(list));
+                from("jetty:http://localhost:8585/shs/ds/urn:X-shs:0000000000.junit")
+                    .process(new Processor() {
+                        @Override
+                        public void process(Exchange exchange) throws Exception {
+                            ShsMessageList list = new ShsMessageList();
+                            for (se.inera.axel.shs.mime.ShsMessage shs : messages.values()) {
+                                if (shs.getLabel().getProduct().getValue().equals(exchange.getIn().getHeader("producttype"))) {
+                                    ShsMessageMarshaller msh = new ShsMessageMarshaller();
+                                    Message m = new Message();
+                                    m.setFrom(shs.getLabel().getFrom().getValue());
+                                    m.setTo(shs.getLabel().getTo().getValue());
+                                    m.setProduct(shs.getLabel().getProduct().getValue());
+                                    m.setTxId(shs.getLabel().getTxId());
+                                    m.setStatus(shs.getLabel().getStatus());
+                                    m.setContentId(shs.getLabel().getContent().getContentId());
+                                    m.setCorrId(shs.getLabel().getCorrId());
 
+                                    list.getMessage().add(m);
+                                }
+                            }
+                            exchange.getIn().setBody(list);
+                        }
+                    })
+                    .convertBodyTo(String.class);
 
+                from("jetty:http://localhost:8585/shs/ds/{shsTo}/{txId}")
+                        .choice()
+                        .when(header(Exchange.HTTP_METHOD).isEqualTo("GET"))
+                        .process(new Processor() {
+                            @Override
+                            public void process(Exchange exchange) throws Exception {
+                                se.inera.axel.shs.mime.ShsMessage shs = messages.get("88B06FFC-860D-430F-B812-A0DA2EB40E9F");
+                                if (shs == null) {
+                                    throw new RuntimeException("message not found");
+                                }
+                                exchange.getIn().setBody(shs);
+                            }
+                        })
+                        .when(PredicateBuilder.and(
+                                header(Exchange.HTTP_METHOD).isEqualTo("POST"),
+                                header("action").isEqualTo("ack")))
+                        .process(new Processor() {
+                            @Override
+                            public void process(Exchange exchange) throws Exception {
+                                messages.remove("88B06FFC-860D-430F-B812-A0DA2EB40E9F");
+                            }
+                    })
+                    .otherwise().throwException(new IllegalArgumentException("Method not allowed"));
             }
-		};
-
-
+        };
 	}
 	
 	@DirtiesContext
 	@Test(enabled = true)
 	public void testShouldThrowException() throws Exception {
-
-//        resultEndpoint.assertIsSatisfied(1000);
-//        Exchange exchange = resultEndpoint.getExchanges().get(0);
-        Thread.sleep(5000);
+        resultEndpoint.expectedMessageCount(1);
+        resultEndpoint.expectedBodiesReceived("Message body");
+        resultEndpoint.expectedHeaderReceived(ShsHeaders.SEQUENCETYPE, SequenceType.EVENT);
+        resultEndpoint.expectedHeaderReceived(ShsHeaders.PRODUCT_ID, "00000000-0000-0000-0000-000000000000");
+        resultEndpoint.expectedHeaderReceived(ShsHeaders.STATUS, Status.TEST);
+        resultEndpoint.expectedHeaderReceived(ShsHeaders.TXID, "88B06FFC-860D-430F-B812-A0DA2EB40E9F");
+        resultEndpoint.expectedHeaderReceived(ShsHeaders.DATAPART_FILENAME, "testfile.txt");
+        resultEndpoint.expectedHeaderReceived(Exchange.FILE_NAME, "88B06FFC-860D-430F-B812-A0DA2EB40E9F");
+        resultEndpoint.assertIsSatisfied(4000);
     }
 	
 
